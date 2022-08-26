aada8e8706683e3211502b7e79f1da5897ee485f
Dhanar Kerja <dhanarkerja@gmail.com>
2022-08-26 20:36:42 +0700

    update table

--- pages/index2.vue
+++ pages/index2.vue
@@ -1,7 +1,17 @@
 <template>
 <div class="container">
     <div class="row d-flex justify-content-center">
-        
+        <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Nama Sparepart</th>
+            <th scope="col">Tanggal</th>
+            <th scope="col">Teknisi</th>
+            <th scope="col">Jumlah</th>
+            <th scope="col">Status</th>
+          </tr>
+        </thead>
+        </table>
     </div>
 </div>
 </template>
